b57d32e28195270a6ec89cacb7758cc8b8dec152
RitaMenezes <38992717+RitaSoaresPCS@users.noreply.github.com>
2018-11-11 18:39:37 -0200

    Adição de importações

--- codigos/src/model/Artigo.java
+++ codigos/src/model/Artigo.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Date;
+
 public class Artigo {
 	
     private Periodico periodico;
--- codigos/src/model/FormacaoAcademica.java
+++ codigos/src/model/FormacaoAcademica.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Date;
+
 public class FormacaoAcademica {
 	
     private Date dataFormacao;
