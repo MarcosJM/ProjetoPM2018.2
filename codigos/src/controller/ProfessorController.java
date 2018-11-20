96eae23e39dbaa0168eba6cf35b983129611c345
Marcos José Moura Pinho Junior <marcos.pinhojr@outlook.com>
2018-11-20 18:11:44 -0200

    criação da classe ProfessorControler que contém função para verificar se determinado professor é da UNIRIO.

---
+++ codigos/src/controller/ProfessorController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import model.ArquivoCsv;
+
+public class ProfessorController {
+	private ArquivoCsv professoresPPGI;
+	
+	public ProfessorController()
+	{
+		this.professoresPPGI = new ArquivoCsv("../../../arquivos/dados/professores_bsi_ppgi.csv", ",");
+	} 
+	public boolean ehProfessorUnirio(String professor)
+	{
+		String[] professores = professoresPPGI.getElementosColuna("Nome Professor");
+		int quantidadeProfessores = professores.length;
+		for (int iterator = 0; iterator < quantidadeProfessores; iterator ++)
+		{
+			if (professores[iterator].equals(professor))
+			{
+				
+				return true;
+			}
+		}
+		return false;
+	}
+}
