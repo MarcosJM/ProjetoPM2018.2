4381c05021bd0d9306f4da5f86d868bee8eb99e6
Mariana Oliveira <mariana.oliveira@uniriotec.br>
2018-11-24 19:07:30 -0200

    Definição dos comandos da interface
    Utilizada a biblioteca picocli para lidar com a questão dos comandos. Foi criada uma classe Comandos para definir todos os comandos e suas variáveis associadas, e, na classe Principal, foi acrescentado o método para capturar os comandos a partir da entrada de dados.

---
+++ codigos/src/view/Comandos.java
@@ -0,0 +1,114 @@
+package view;
+
+import utils.CommandLine.Option;
+import utils.CommandLine.Parameters;
+
+import java.util.List;
+
+/**
+ * Classe para definir os comandos (conceitualmente, tido como opcoes) aceitos pelo sistema e suas respectivas variaveis associadas. 
+ * Essa classe eh implementada utilizando-se da classe CommandLine, que contem o codigo da biblioteca picocli.
+ * 
+ * Os comandos suportados sao:
+ * 
+ * 1) -a <nome-caminho-arquivo-xml> <dado-1> : indica caminho do arquivo com o currículo Lattes em XML e, nos parametros, os dados do candidato a ser considerado. Obrigatorio.
+ * 2) -o <nome-caminho-arquivo-txt-saida> : indica o caminho do arquivo texto que deve conter a saída do programa. Obrigatorio.
+ * 3) -l <nome-caminho-arquivo-log-errors> : indica o caminho do arquivo texto que deve conter um log/relatório de erros do programa. Opcional.
+ * 4) -v : modificador que indica que a saída deve ser completa (modo verboso, ou verbose mode). Opcional.
+ * 5) -c : gera a saída completa do programa. É equivalente a usar, em conjunto, os parâmetros -pr -ar -anr -pe -vi. Por padrão, eh o considerado.
+ * 6) -pr : gera a saída referente aos prêmios recebidos. 
+ * 7) -ar : gera a saída referente aos artigos completos no Qualis Restrito (ou seja, publicados em conferências ou periódicos classificados como A1, A2 e B1).
+ * 8) -anr : gera a saída referente aos artigos completos fora do Qualis Restrito (ou seja, publicados em conferências ou periódicos classificados como B2, B3, B4 e B5).
+ * 9) -pe : gera a saída referente à participação em eventos classificados (ou seja, referentes a conferências classificadas como A1, A2, B1, B2, B3, B4 e B5).
+ * 10) -vi : gera a saída referente à existência de vínculo com a UNIRIO.
+ * 
+ * A biblioteca picocli assume qualquer ordem de comandos. Assim tambem como aceita concatenacoes (e.g. -c -f, -cf).
+ * 
+ * @author Mariana
+ *
+ */
+
+public class Comandos {
+	
+	@Option(names = "-a", description = "Indica os dados do candidato a ser considerado e o caminho do arquivo XML.", required = true)
+	private String caminhoEntrada;
+	
+	@Parameters(description = "Podem ser passados varios parametros para o comando -a quanto aos dados a serem considerados.")
+	List<String> dadosConsiderados;
+	
+	@Option(names = "-o", description = "Caminho do arquivo texto que contem a saida do programa.", required = true)
+	private String caminhoSaida;
+
+	@Option(names = "-l", description = "Caminho do arquivo texto que contem um log de erros do programa.")
+	private String caminhoLog;
+	
+	@Option(names = "-v", description = "Modo detalhado. Descricao completa da execucao do codigo.")
+	private boolean verbose;
+	
+	@Option(names = "-c", description = "Gera a saida completa do programa.")
+	private boolean completo;
+	
+	@Option(names = "-pr", description = "Gera a saida referente aos premios recebidos.")
+	private boolean premios;
+	
+	@Option(names = "-ar", description = "Gera a saida referente aos os artigos completos no Qualis Restrito (A1, A2 e B1).")
+	private boolean artigosNoQualisRestrito;
+	
+	@Option(names = "-anr", description = "Gera a saida referente aos os artigos completos fora do Qualis Restrito (B2, B3, B4 e B5).")
+	private boolean artigosForaQualisRestrito;
+	
+	@Option(names = "-pe", description = "Gera a saida referente a participacao em eventos classificados (A1, A2, B1, B2, B3, B4 e B5).")
+	private boolean eventosClassificados;
+	
+	@Option(names = "-vi", description = "Gera a saida referente a existencia de vinculo com a UNIRIO.")
+	private boolean vinculoUnirio;
+	
+	
+	/**
+	 *  Abaixo, metodos para retornar as variaveis associadas a cada opcao.
+	 */
+	
+	public String getCaminhoEntrada() {
+		return caminhoEntrada;
+	}
+	
+	public List<String> getDadosConsiderados() {
+		return dadosConsiderados;
+	}
+	
+	public String getCaminhoSaida() {
+		return caminhoSaida;
+	}
+	
+	public String getCaminhoLog() {
+		return caminhoLog;
+	}
+	
+	public boolean isVerbose() {
+		return verbose;
+	}
+	
+	public boolean isCompleto() {
+		return completo;
+	}
+	
+	public boolean isPremios() {
+		return premios;
+	}
+	
+	public boolean isArtigosNoQualisRestrito() {
+		return artigosNoQualisRestrito;
+	}
+	
+	public boolean isArtigosForaQualisRestrito() {
+		return artigosForaQualisRestrito;
+	}
+	
+	public boolean isEventosClassificados() {
+		return eventosClassificados;
+	}
+	
+	public boolean isVinculoUnirio() {
+		return vinculoUnirio;
+	}
+}
--- codigos/src/view/Principal.java
+++ codigos/src/view/Principal.java
@@ -1,43 +1,66 @@
 package view;
 
+import utils.XmlUtils;
+import utils.CommandLine;
+
+import static java.lang.System.out;
+
+import java.util.Scanner;
 
 /**
  * Interface com o usuário.
  *
  */
+
 public class Principal {
-
-	public static void main(String[] args) {
-		// Leitura de um XML.
-		
-		// 
-
+	
+	/**
+	 * Funcao para decompor a entrada passada em argumentos.
+	 * Isso eh feito ao cortar a string, utilizando espaco como separador.
+	 * 
+	 * @param entrada
+	 * @return argumentos
+	 */
+	public String[] decompoeEntrada(String entrada) {
+		String[] argumentos = entrada.split(" ");
+		return argumentos;
 	}
 	
-	/*
+	/**
+	 * Funcao que, de acordo com o comando, redireciona para a chamada de funcao adequada.
+	 * 
+	 * A relacao de cada comando com sua execucao esperada eh dada por:
+	 * 
+	 * 1) -a : leitura do arquivo XML do lattes e definicao dos dados a serem julgados.
+	 * 2) -o <nome-caminho-arquivo-txt-saida> : gravacao de arquivo texto com saida do programa.
+	 * 3) -l <nome-caminho-arquivo-log-errors> : gravacao de arquivo texto com erros do programa.
+	 * 4) -v : modo verbose, exibe a execucao do programa.
+	 * 5) -c : conteudo da saida do programa eh completo.
+	 * 6) -pr : para cada candidato, deve ser exibida a quantidade de prêmios considerados e a pontuação obtida.
+	 * 7) -ar : para cada candidato, deve ser exibida a quantidade de artigos considerados e a pontuação obtida,
+	 * 		Se o modo verbose ativado, deve ser exibida ao final, para cada candidato: o ano de publicação, nome do artigo, conferência ou periódico associado e o Qualis associado.
+	 * 8) -anr : para cada candidato, deve ser exibida a quantidade de artigos considerados e a pontuação obtida,
+	 * 		Se o modo verboso estiver ativado, deve ser exibida ao final, para cada candidato: o ano de publicação, nome do artigo, conferência ou periódico associado e o Qualis associado.
+	 * 9) -pe : para cada candidato, deve ser exibida a quantidade de participações consideradas e a pontuação obtida.
+	 * 		Se o modo verboso estiver ativado, deve ser exibida ao final, para cada candidato: o ano, conferência e o Qualis associado.
+	 * 10) -vi : para cada candidato, deve ser exibida a quantidade de vínculos considerados e a pontuação obtida.
+	 * 		Se o modo verboso estiver ativado, deve ser exibida ao final, para cada candidato: o ano, vínculo associado.
+	 * 
+	 * @param argumentos
+	 * 
+	 */
+	public void executaComando(String[] argumentos) {
+		Comandos comandos = new Comandos();
+		
+		Comandos executar = CommandLine.populateCommand(comandos, argumentos);
+	}
 	
-	-o <nome-caminho-arquivo-txt-saida> : indica o caminho do arquivo texto que deve conter a saída do programa.
-	-l <nome-caminho-arquivo-log-errors> : indica o caminho do arquivo texto que deve conter um log/relatório de erros do programa.
-	-a <nome-caminho-arquivo-xml> <num-semestres-sem-reprovacao>: indica os dados do candidato a ser considerado: o caminho do arquivo com o currículo Lattes, no formato XML, e o número de semestres que o aluno já completou sem reprovações. Podem ser passados vários parâmetros "-a" para o programa.
-	-c : gera a saída completa do programa.
-	É equivalente a usar, em conjunto, os parâmetros -pr -ar -anr -pe -vi.
-	-v : modificador que indica que a saída deve ser completa (modo verboso, ou verbose mode).
-	-pr : gera a saída referente aos prêmios recebidos.
-	Para cada candidato, deve ser exibida a quantidade de prêmios considerados e a pontuação obtida.
-	Se o modo verboso estiver ativado, deve ser exibida ao final, para cada candidato, o ano e o nome do prêmios obtidos.
-	-ar : gera a saída referente aos artigos completos no Qualis Restrito (ou seja, publicados em conferências ou periódicos classificados como A1, A2 e B1).
-	Para cada candidato, deve ser exibida a quantidade de artigos considerados e a pontuação obtida.
-	Se o modo verboso estiver ativado, deve ser exibida ao final, para cada candidato: o ano de publicação, nome do artigo, conferência ou periódico associado e o Qualis associado.
-	-anr : gera a saída referente aos artigos completos fora do Qualis Restrito (ou seja, publicados em conferências ou periódicos classificados como B2, B3, B4 e B5).
-	Para cada candidato, deve ser exibida a quantidade de artigos considerados e a pontuação obtida.
-	Se o modo verboso estiver ativado, deve ser exibida ao final, para cada candidato: o ano de publicação, nome do artigo, conferência ou periódico associado e o Qualis associado.
-	-pe : gera a saída referente à participação em eventos classificados (ou seja, referentes a conferências classificadas como A1, A2, B1, B2, B3, B4 e B5).
-	Para cada candidato, deve ser exibida a quantidade de participações consideradas e a pontuação obtida.
-	Se o modo verboso estiver ativado, deve ser exibida ao final, para cada candidato: o ano, conferência e o Qualis associado.
-	-vi : gera a saída referente à existência de vínculo com a UNIRIO.
-	Para cada candidato, deve ser exibida a quantidade de vínculos considerados e a pontuação obtida.
-	Se o modo verboso estiver ativado, deve ser exibida ao final, para cada candidato: o ano, vínculo associado.
-
-*/
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("$ ");
+		String entrada = scanner.next();
+		
+	}
 
 }
